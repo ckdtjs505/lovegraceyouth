16dfb97850790fa3c8a8e970b43f6c2402fa3035
ckdtjs505 <ckdtjs505@naver.com>
2021-07-05 22:41:16 +0900

    feat : set express

---
+++ index.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "sonia", message: "helloff" });
+});
+
+app.get("/love", (req, res) => {
+  res.send("Hello love wrwr");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
